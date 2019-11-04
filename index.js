4e8bbbafb7c18af3d28f35dc9125962f2ffc6717
Trevor Bennett <github@trevorbennett.us>
2019-11-04 10:38:26 -0500

    update to output all invalid forms of ssn with potential digits for the format. serial output is still a wip

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 
 const SPECIAL_SOCIAL_SECURITY_NUMBERS = ["01-01-0001","078-05-1120","457-55-5462"]
 
+let invalidAreaNumbers = ["0","666"]
+// for(let step = 900;step<999;step++){
+//     invalidAreaNumbers.push(step.toString());
+// }
+
+let invalidGroupNumbers = ["0"]
+
+let invalidSerialNumbers = ["0"]
+
+
+
 let validAreaNumbers = [];
 for(let step = 1; step < 900; step++) {
     if(step != 666){
@@ -24,12 +35,60 @@ for(let step = 1; step < 10000; step++) {
 let output = ""
 
 
-
-for(areaNumber of validAreaNumbers){
-    let filename = areaNumber+".txt"
+//invalid area numbers - IVV, IVI, IIV, III
+for(areaNumber of invalidAreaNumbers){
+    let filename = areaNumber.padStart(3, '0')+"-invalid-social-security-numbers.txt"
     output = ""
     for(groupNumber of validGroupNumbers){
         for(serialNumber of validSerialNumbers){
+            output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+        }
+        for(serialNumber of invalidSerialNumbers){
+            output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+        }
+    }
+    for(groupNumber of invalidGroupNumbers){
+        for(serialNumber of validSerialNumbers){
+            output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+        }
+        for(serialNumber of invalidSerialNumbers){
+            output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+        }
+    }
+
+    fs.writeFile(filename, output, (err) => {
+        if (err) throw err;
+    
+        console.log("Saved: "+filename);
+    });
+}
+
+//invalid group numbers VIV, VII
+for(areaNumber of validAreaNumbers){
+    let filename = areaNumber.padStart(3, '0')+"-bad-group-invalid-social-security-numbers.txt"
+    output = ""
+    for(groupNumber of invalidGroupNumbers){
+        for(serialNumber of validSerialNumbers){
+            output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+        }
+        for(serialNumber of invalidSerialNumbers){
+            output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+        }
+    }
+
+    fs.writeFile(filename, output, (err) => {
+        if (err) throw err;
+    
+        console.log("Saved: "+filename);
+    });
+}
+
+//invalid serial numbers - VVI
+for(areaNumber of validAreaNumbers){
+    let filename = "0000-serial-invalid-social-security-numbers.txt"
+    output = ""
+    for(groupNumber of validGroupNumbers){
+        for(serialNumber of invalidSerialNumbers){
             output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
         }
     }
@@ -42,3 +101,30 @@ for(areaNumber of validAreaNumbers){
 }
 
 
+//valid ssns - VVV
+// for(areaNumber of validAreaNumbers){
+//     let filename = areaNumber.padStart(3, '0')+"-invalid-social-security-numbers.txt"
+//     output = ""
+//     for(groupNumber of validGroupNumbers){
+//         for(serialNumber of validSerialNumbers){
+//             output += areaNumber.padStart(3, '0')+"-"+groupNumber.padStart(2, '0')+"-"+serialNumber.padStart(4, '0')+"\n"
+//         }
+//     }
+
+//     fs.writeFile(filename, output, (err) => {
+//         if (err) throw err;
+    
+//         console.log("Saved: "+filename);
+//     });
+// }
+
+//speacial SSNs, hard coded
+output = ""
+for(ssn of SPECIAL_SOCIAL_SECURITY_NUMBERS){
+output+=ssn+"\n"
+}
+fs.writeFile("special-social-security-numbers.txt", output, (err) => {
+    if (err) throw err;
+
+    console.log("Saved: "+"special-social-security-numbers.txt");
+});
